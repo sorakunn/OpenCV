6a89abd457d2ee9546a7418b2625f7f5d571b489
zlw <sorakunn@users.noreply.github.com>
2017-07-14 21:17:32 +0800

    Update Basic_ImageCanny.cpp

--- OpenCV3/1.Basic/Basic_ImageCanny.cpp
+++ OpenCV3/1.Basic/Basic_ImageCanny.cpp
@@ -1,5 +1,5 @@
-#include<opencv2/highgui/highgui.hpp>										/* 测试形态学运算——“Canny边缘检测”(将载入图像转为灰度图并检测边缘) */
-#include<opencv2/imgproc/imgproc.hpp>										/* Canny函数 */
+#include<opencv2/highgui/highgui.hpp>				/* 测试形态学运算——“Canny边缘检测”(将载入图像转为灰度图并检测边缘) */
+#include<opencv2/imgproc/imgproc.hpp>				/* Canny函数 */
 using namespace cv;
 int main(){
 	Mat srcImage = imread("test.jpg");
@@ -10,9 +10,9 @@ int main(){
 	dstImage.create( srcImage.size(), srcImage.type() );
   
 	//进行边缘检测	【转灰度→降噪→Canny函数】
-	cvtColor(srcImage,grayImage,COLOR_BGR2GRAY);				//将图像转换为灰度图像
-	blur(grayImage,edge,Size(3,3));								      //使用 3x3 内核来降噪
-	Canny(edge,dstImage,3,9,3);								        	//运行Canny函数
+	cvtColor(srcImage,grayImage,COLOR_BGR2GRAY);		//将图像转换为灰度图像
+	blur(grayImage,edge,Size(3,3));				//使用 3x3 内核来降噪
+	Canny(edge,dstImage,3,9,3);				//运行Canny函数
 
 	imshow("效果图【canny边缘检测】",dstImage);
 	waitKey(0);
