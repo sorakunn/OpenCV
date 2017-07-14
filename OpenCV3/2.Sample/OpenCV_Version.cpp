f2151445b0c1f6fdac9f64d7277af3259fdbd07a
zlw <sorakunn@users.noreply.github.com>
2017-07-14 21:13:17 +0800

    Update OpenCV_Version.cpp

--- OpenCV3/2.Sample/OpenCV_Version.cpp
+++ OpenCV3/2.Sample/OpenCV_Version.cpp
@@ -1,10 +1,11 @@
 #include <opencv2/opencv.hpp>			/* 该程序用来显示当前OpenCV版本 */
-using namespace cv;				/* printf函数, cout函数 */												/* printf函数, cout函数 */
+using namespace cv;				/* printf函数, cout函数 */
 using namespace std;                            //使用C++风格输出时不要忘了  using namespace std;
 
 void main(){
 	printf("当前使用的OpenCV版本为：OpenCV"CV_VERSION);	   //C风格	prinf("%d",num);
-	cout<<endl;						  //C++风格	cout<<"版本为："<<CV_VERSION<<"版";										             	//C++风格		cout<<"版本为："<<CV_VERSION<<"版"	
+	cout<<endl;						  //C++风格	cout<<"版本为："<<CV_VERSION<<"版";	
+
 }
 
 /*	c++的基本输入语句：
