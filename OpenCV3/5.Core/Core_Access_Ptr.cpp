ba6acaa63446c86d8ac02f45eb11209b5a42d039
zlw <sorakunn@users.noreply.github.com>
2017-07-14 20:58:05 +0800

    Update Core_Access_Ptr.cpp

--- OpenCV3/5.Core/Core_Access_Ptr.cpp
+++ OpenCV3/5.Core/Core_Access_Ptr.cpp
@@ -1,6 +1,6 @@
-#include <opencv2/opencv.hpp>								/* 访问图像中像素（方法一） */
-using namespace cv;											/* Mat.rows成员, Mat.cols成员 */
-															/* Mat.channels()方法,Mat.ptr<type>()方法 */
+#include <opencv2/opencv.hpp>					/* 访问图像中像素（方法一） */
+using namespace cv;						/* Mat.rows成员, Mat.cols成员 */
+								/* Mat.channels()方法,Mat.ptr<type>()方法 */
 
 void colorReduce(Mat& srcImage, Mat& dstImage, int div){	//注意是引用传递“&”
 	dstImage = srcImage.clone();
@@ -8,9 +8,9 @@ void colorReduce(Mat& srcImage, Mat& dstImage, int div){	//注意是引用传递
 	int colNumber = dstImage.cols * dstImage.channels();
 
 	for(int i=0; i<rowNumber; i++){
-		uchar* p = dstImage.ptr<uchar>(i);					//ptr函数用于取得任意行的首地址
+		uchar* p = dstImage.ptr<uchar>(i);		//ptr函数用于取得任意行的首地址
 		for(int j=0; j<colNumber; j++){
-			*p++ = *p/div*div;								//用指针运算和自增代替[]操作符
+			*p++ = *p/div*div;			//用指针运算和自增代替[]操作符
 		}
 	}
 }
